6038505be934bc68c3b2825d6b4d601cc4574890
Federica Mantica <federica.mantica93@gmail.com>
2021-03-30 12:22:53 +0200

    multiple alignment now works for batches

--- src/py/align_chimeric_OG.py
+++ src/py/align_chimeric_OG.py
@@ -14,16 +14,16 @@ import os
 parser = argparse.ArgumentParser(description="Generate multiple alignments among all the proteins in an orthogroup containing chimeric genes")
 parser.add_argument("--input_fastas", "-f", required=True, metavar="input_fastas", help="directory containing all proteome fasta files of species in orthogroups")
 parser.add_argument("--input_OG", "-i", required=True, metavar="input_orthogroups", help="orthogroups containing chimeric genes")
-parser.add_argument("--chimeric_gene", "-g", required=True, metavar="chimeric_gene", help="geneID of a chimeric gene as reported in the fasta files")
+parser.add_argument("--chimeric_genes", "-g", required=True, metavar="chimeric_gene", help="comma-separated list of chimeric geneIDs for which to perform the alignment in the relative orthogroup")
 parser.add_argument("--mafft", "-m", required=True, metavar="mafft_executable", help="path to mafft execution file")
-parser.add_argument("--output", "-o", required=True, metavar="output_file", help="path to output file")
+parser.add_argument("--output", "-o", required=True, metavar="output_dir", help="path to output dir")
 
 
 #Read arguments
 args = parser.parse_args()
 my_input_fastas = args.input_fastas
 my_input_OG = args.input_OG
-my_chimeric_gene = args.chimeric_gene
+my_chimeric_genes = args.chimeric_genes.split(",") #split the original batch
 my_mafft = args.mafft
 my_output = args.output
 
@@ -39,15 +39,18 @@ for my_file in all_fastas_files: #cycle on all the fasta files
     element.description = ""
   all_fastas_entries = all_fastas_entries+my_fasta
 
-my_OG_ID = list(chimeric_OG_df[chimeric_OG_df["geneID"]==my_chimeric_gene]["OG_ID"])[0]
-OG_df = chimeric_OG_df[chimeric_OG_df["OG_ID"]==my_OG_ID]
-#I still need to remove all the other chimeric genes from the orthogroup.
-OG_genes = list(OG_df["geneID"])
-fastas_entries = [element for element in all_fastas_entries if element.id in OG_genes]
-#save fastas entries to temporary file
-with open('input_fasta_tmp.fa', 'w') as input_to_aln:
-  SeqIO.write(fastas_entries, input_to_aln, "fasta")
-#call shell command to run the mafft alignment
-my_command = "%s --quiet --retree 2 --localpair --maxiterate 1000 input_fasta_tmp.fa > %s" % (my_mafft, my_output)
-print(my_command)
-os.system(my_command)
+for my_chimeric_gene in my_chimeric_genes:
+  print(my_chimeric_gene) 
+  my_OG_IDs = list(set(list(chimeric_OG_df[chimeric_OG_df["geneID"]==my_chimeric_gene]["OG_ID"])))
+  for my_OG_ID in my_OG_IDs:
+    OG_df = chimeric_OG_df[chimeric_OG_df["OG_ID"]==my_OG_ID]
+    #I still need to remove all the other chimeric genes from the orthogroup.
+    OG_genes = list(OG_df["geneID"])
+    fastas_entries = [element for element in all_fastas_entries if element.id in OG_genes]
+    #save fastas entries to temporary file
+    with open('input_fasta_tmp.fa', 'w') as input_to_aln:
+      SeqIO.write(fastas_entries, input_to_aln, "fasta")
+    #call shell command to run the mafft alignment
+    my_command = "%s --quiet --retree 2 --localpair --maxiterate 1000 input_fasta_tmp.fa > %s/%s-%s-multiple_aln" % (my_mafft, my_output, my_chimeric_gene, my_OG_ID)
+    print(my_command)
+    os.system(my_command)
