b2abffe0cdf29f875059398d4ee2ba74d05bc7a7
Federica Mantica <federica.mantica93@gmail.com>
2021-04-02 11:47:23 +0200

    Working on get_exon_aa_coords.py

--- src/py/get_exon_aa_coords.py
+++ src/py/get_exon_aa_coords.py
@@ -2,7 +2,7 @@
 
 import argparse
 import pandas as pd
-import glob
+import re
 import math
 
 parser = argparse.ArgumentParser(description="Script to compute the aminoacidid portion coded by each exon")
@@ -22,11 +22,34 @@ ref_prot_df = pd.read_table(ref_prot_file, sep="\t", index_col=False, header=Non
 gtf_CDS = gtf[(gtf["type"]=="CDS") & (gtf["attribute"].str.contains("protein_id"))]
 
 #Add columns with relevant information
-gtf_CDS["geneID"] = [re.sub(".*[ ]", "", re.sub('"', "", element.split(";")[0])) for element in list(gtf_CDS["attribute"])]
-gtf_CDS["proteinID"] = [part for element in list(gtf_CDS["attribute"]) for part in element.split(";") if "protein_id" in part] #This ugly thing is necessary from when the proteinID is repeated in the GTF
-gtf_CDS["proteinID"] = [re.sub(".*[ ]", "", re.sub('"', "", element)) for element in list(gtf_CDS["proteinID"])]
-gtf_CDS["exon_number"]  = [int(re.sub(".*[ ]", "", re.sub('"', "", part))) for element in list(gtf_CDS["attribute"]) for part in element.split(";") if "exon_number" in part]
-gtf_CDS["phase"] = pd.to_numeric(gtf_CDS["phase"])
+gtf_CDS["gene_id"] = [re.sub(".*[ ]", "", re.sub('"', "", part)) for element in list(gtf_CDS["attribute"]) for part in element.split(";") if "gene_id" in part]
+if len([element for element in  list(gtf_CDS["attribute"])[0].split(";") if "protein_id" in element]) == 2:
+  gtf_CDS["proteinID"] = [re.sub(".*[ ]", "", re.sub('"', "", element.split(";")[0])) for element in list(gtf_CDS["attribute"])] 
+else:
+  gtf_CDS["proteinID"] = [re.sub(".*[ ]", "", re.sub('"', "", part)) for element in list(gtf_CDS["attribute"]) for part in element.split(";") if "protein_id" in part]
+
+#Create a new columns with the last sub-field of the attribute field
+#gtf_CDS["last_field"] = [part for element in list(gtf_CDS["attribute"]) for part in element.split(";") if "protein_ID" in part]
+
+#gtf_CDS["last_field"] = [element.split(";")[-2] for element in list(gtf_CDS["attribute"])]
+#This is when the proteinID is the last entry
+#protein_id_last = [element for element in list(gtf_CDS["last_field"]) if "protein_id" in element]
+#gtf_protein_id_last = gtf_CDS[gtf_CDS["last_field"].isin(protein_id_last)]
+#gtf_protein_id_last["protein_id"] = [re.sub(".*[ ]", "", re.sub('"', "", element)) for element in list(gtf_protein_id_last["last_field"])]
+#This is when the proteinID is NOT the last entry
+#gtf_NOprotein_id_last = gtf_CDS[~(gtf_CDS["last_field"].isin(protein_id_last))]
+#gtf_NOprotein_id_last["protein_id"] = [re.sub(".*[ ]", "", re.sub('"', "", part)) for element in list(gtf_CDS["attribute"]) for part in element.split(";") if "protein_id" in part] 
+#Update
+#gtf_CDS = pd.concat([gtf_protein_id_last, gtf_NOprotein_id_last])
+
+#Horrible code, but life is complicated. This is necessary for when the proteinID is repeated in the GTF.
+#if len([element for element in  list(gtf_CDS["attribute"])[0].split(";") if "protein_id" in element]) == 2:
+#  raw_protein = [part for element in list(gtf_CDS["attribute"]) for part in element.split(";")[:-2] if "protein_id" in part] #I simply remove the last entry in the attribute column where the proteinID is repeated.
+#else:
+#  raw_protein = [part for element in list(gtf_CDS["attribute"]) for part in element.split(";") if "protein_id" in part]
+#gtf_CDS["proteinID"] = [re.sub(".*[ ]", "", re.sub('"', "", element)) for element in raw_protein]
+#gtf_CDS["exon_number"]  = [int(re.sub(".*[ ]", "", re.sub('"', "", part))) for element in list(gtf_CDS["attribute"]) for part in element.split(";") if "exon_number" in part]
+#gtf_CDS["phase"] = pd.to_numeric(gtf_CDS["phase"])
 
 #Filter only for ref proteins
 gtf_CDS_ref = gtf_CDS[gtf_CDS["proteinID"].isin(list(ref_prot_df["ref_prot_ID"]))]
