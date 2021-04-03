6dd49bbe3558b9d345fe9268ddc96a6a949fb55a
Federica Mantica <federica.mantica93@gmail.com>
2021-04-04 01:15:26 +0200

    Some changes for cluster submission

--- src/sh/snakemake_default.sh
+++ src/sh/snakemake_default.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
-snakemake -j 10 --use-conda --cluster-config cluster.json --cluster "{cluster.qsub} -l h_vmem={cluster.ram},h_rt={cluster.time},disk={cluster.disk} -q {cluster.queue}" --keep-going --latency-wait 100
+#$1 reads the first argument from the command line
+
+snakemake -j $1 --use-conda --cluster-config cluster.json --cluster "{cluster.qsub} -l h_vmem={cluster.ram},h_rt={cluster.time},disk={cluster.disk} -q {cluster.queue}" --keep-going --latency-wait 100
 #--restart-times 2 --rerun-incomplete
